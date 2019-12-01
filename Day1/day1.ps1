fc4a8b88738d5ca36d8fdce5aebee9104482e9ae
Daniel Winship <danielwinship@icloud.com>
2019-12-01 07:39:54 -0500

    solution

--- Day1/day1.ps1
+++ Day1/day1.ps1
@@ -1,1 +1,9 @@
-Nothing here yet
+$numbers = Get-Content ./puzzleinput.txt
+
+foreach($number in $numbers) {
+  $numberDivided = $number / 3
+  $numberDivided = [math]::Floor($numberDivided)
+  $numberSubtracted = $numberDivided - 2
+  $finalNumber += $numberSubtracted
+}
+$finalNumber
